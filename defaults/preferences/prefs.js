74c3a7c3c8fdf1cdd93972eda6de504ec63adb5f
Victor Pantoja <victor.pantoja@corp.globo.com>
2010-10-30 20:56:26 -0200

    Refactory

--- defaults/preferences/prefs.js
+++ defaults/preferences/prefs.js
@@ -3,4 +3,4 @@ pref("extensions.cartolaextension.intpref", 0);
 pref("extensions.cartolaextension.stringpref", "A string");
 
 // https://developer.mozilla.org/en/Localizing_extension_descriptions
-pref("extensions.cartolafc@cartolafc.com.description", "chrome://cartolaextension/locale/overlay.properties");
+pref("extensions.cartolafc@cartolafc.com.description", "chrome://cartolaextension/locale/cartola-addon.properties");
