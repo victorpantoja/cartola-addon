617219c0417c1835beb010d1bbd30c5c75b12f88
Victor Pantoja <victor.pantoja@corp.globo.com>
2010-11-27 00:40:49 -0200

    Preferencias

--- defaults/preferences/prefs.js
+++ defaults/preferences/prefs.js
@@ -1,4 +1,7 @@
 pref("extensions.cartolanotifier.currentUser", "");
+pref("extensions.cartolanotifier.interval", 5);
+pref("extensions.cartolanotifier.countdown", true);
+
 
 // https://developer.mozilla.org/en/Localizing_extension_descriptions
 pref("extensions.cartolafc@cartolafc.com.description", "chrome://cartolaextension/locale/cartola-addon.properties");
